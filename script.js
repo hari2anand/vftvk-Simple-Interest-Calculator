5ce6deb275e195c3890381218113674af218d91d
Harish Anand Thiru Kumareshan <harishanand.thiru.kumareshan@ingka.ikea.com>
2021-02-09 23:47:01 +0100

    rebuild pages

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 function compute(){
- //function for computing the Intrest - Prepared for Exercise 4
     var principal = document.getElementById("principal").value;
-    //To validate the prinicpal not goes below 0
     if (principal<=0){
       document.getElementById("result").innerHTML = "Please enter a valid amount.";
       return;
@@ -13,7 +11,6 @@ function compute(){
     var year = currentDate.getFullYear();
     var maturityYear = parseInt(years) + parseInt(year);
     var maturityValue = parseInt(interest) + parseInt(principal);
-    //Give customer a headsup on what he's gona pay for his life :lol
     var output = "If you deposit " + principal +"<br>at an interest rate of " + rate +"%.<br>You will receive an amount of " + maturityValue + "<br> at year " + maturityYear + ".<br>";
     document.getElementById("result").innerHTML = output;
 }
