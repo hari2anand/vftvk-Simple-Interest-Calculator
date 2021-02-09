750b2bf1574fe3cd133b9c65b61ffd755ed2cb0d
Harish Anand Thiru Kumareshan <45207514+hari2anand@users.noreply.github.com>
2021-02-09 23:32:22 +0100

    Lab Exercises update (#1)
    * updated as per the lab requirement
    
    * updates as per Exercise3
    
    * update for Lab Exercise4

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){
+ //function for computing the Intrest - Prepared for Exercise 4
+    var principal = document.getElementById("principal").value;
+    //To validate the prinicpal not goes below 0
+    if (principal<=0){
+      document.getElementById("result").innerHTML = "Please enter a valid amount.";
+      return;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = (parseInt(principal) * parseFloat(rate) * parseInt(years))/100;
+    var currentDate = new Date();
+    var year = currentDate.getFullYear();
+    var maturityYear = parseInt(years) + parseInt(year);
+    var maturityValue = parseInt(interest) + parseInt(principal);
+    //Give customer a headsup on what he's gona pay for his life :lol
+    var output = "If you deposit " + principal +"<br>at an interest rate of " + rate +"%.<br>You will receive an amount of " + maturityValue + "<br> at year " + maturityYear + ".<br>";
+    document.getElementById("result").innerHTML = output;
 }
-        
+function displayRate(){
+    var rate = document.getElementById("rate").value;
+    rate += "%";
+    document.getElementById("displayRate").innerHTML = rate;
+}
